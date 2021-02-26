353e42a60b3f75e107734bf76914812e049e5996
Vuochlang <72162972+Vuochlang@users.noreply.github.com>
2021-02-26 15:27:28 -0800

    Update sub.c

--- week7/sub.c
+++ week7/sub.c
@@ -1,29 +1,11 @@
-/***********************************************************************
-name:
-	assignment4 -- acts as a pipe using ":" to seperate programs.
-description:	
-	See CS 360 Processes and Exec/Pipes lecture for helpful tips.
-***********************************************************************/
-
-/* Includes and definitions */
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <assert.h>
 
-/**********************************************************************
-./assignment4 <arg1> : <arg2>
-
-    Where: <arg1> and <arg2> are optional parameters that specify the programs
-    to be run. If <arg1> is specified but <arg2> is not, then <arg1> should be
-    run as though there was not a colon. Same for if <arg2> is specified but
-    <arg1> is not.
-**********************************************************************/
-
 int main(int argc, char *argv[]){
-	assert(argc % 2 == 1);  // even number of argv
+	assert(argc % 2 == 1); // even number of command line arguments
 
 	int fd[2];
 	int reader, writer;
@@ -38,7 +20,6 @@ int main(int argc, char *argv[]){
 	    while(read(0, buffer, 1)) {
 	        write(writer, buffer, 1);
 	    }
-//	    close(writer);
 	    wait(0);  // wait for the child to finish
         close(writer);
 	}
